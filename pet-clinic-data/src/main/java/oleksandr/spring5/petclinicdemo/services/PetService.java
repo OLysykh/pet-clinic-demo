f37b83e3f725665c8658a00d47b5bb019b5ac5fc
OLysykh <a.lyssykh@gmail.com>
2022-10-11 22:04:20 +0300

    mimic CRUD<> interface

--- pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/services/PetService.java
+++ pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/services/PetService.java
@@ -1,13 +1,11 @@
 package oleksandr.spring5.petclinicdemo.services;
 
+import oleksandr.spring5.petclinicdemo.model.Pet;
+
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long>{
     Pet findByLastName(String lastName);
 
-    Pet findById(Long id);
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
