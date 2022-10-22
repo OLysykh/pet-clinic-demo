4f10abad0abc9de0eae9e78a7e99df1b00f480fb
OLysykh <a.lyssykh@gmail.com>
2022-10-22 11:29:00 +0300

    jpa mapping!

--- pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/model/Person.java
+++ pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/model/Person.java
@@ -1,7 +1,13 @@
 package oleksandr.spring5.petclinicdemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
@@ -19,4 +25,6 @@ public class Person extends BaseEntity{
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+
 }
--- pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/model/Speciality.java
+++ pet-clinic-data/src/main/java/oleksandr/spring5/petclinicdemo/model/Speciality.java
@@ -1,6 +1,13 @@
 package oleksandr.spring5.petclinicdemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="specialties")
 public class Speciality extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
